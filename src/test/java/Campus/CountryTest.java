a6c9364527231e4f383ac13747dff9b84a9a7b9f
gcamp <g.campinari@hotmail.com>
2023-01-16 12:45:37 +0100

    RestAssured 4 - 1

---
+++ src/test/java/Campus/CountryTest.java
@@ -0,0 +1,196 @@
+package Campus;
+
+import Campus.Model.Country;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class CountryTest {
+
+    Cookies cookies;
+    String countryID;
+    String countryName;
+    String countryCode;
+
+    @BeforeClass
+    public void loginCampus()
+    {
+        baseURI="https://test.mersys.io/";
+        // diger testler calismadan önce login olup cookies i alinmasi gerekiyor
+        // bu yüzden beforeClass annototion i eklendi
+
+        Map<String,String> credendial=new HashMap<>();
+        credendial.put("username", "turkeyts");
+        credendial.put("password", "TechnoStudy123");
+        credendial.put("rememberMe", "true");
+
+        cookies=
+        given()
+                .contentType(ContentType.JSON)
+                .body(credendial)
+
+                .when()
+                .post("auth/login")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract().response().getDetailedCookies()
+        ;
+
+        System.out.println("cookies = " + cookies);
+
+    }
+
+    public String getRandomName() { return RandomStringUtils.randomAlphabetic(8); }
+    public String getRandomCode() { return RandomStringUtils.randomAlphabetic(3); }
+
+    @Test
+    public void createCountry()
+    {
+        countryName= getRandomName();
+        countryCode= getRandomCode();
+
+        Country country= new Country();
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        countryID=
+        given()
+
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .post("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().jsonPath().getString("id")
+
+        ;
+    }
+
+
+    @Test(dependsOnMethods = "createCountry")
+    public void createCountryNegative()
+    {
+        Country country= new Country();
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+
+        given()
+
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .post("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("The Country with Name \"" + countryName + "\" already exists."))
+                .body("message", containsString("already exists"))
+        ;
+    }
+
+    @Test(dependsOnMethods = "createCountry", priority = 2)
+    public void updateCountry() {
+        countryName = getRandomName();
+        countryCode = getRandomCode();
+
+        Country country = new Country();
+        country.setId(countryID);
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .put("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(countryName))
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateCountry")
+    public void deleteCountryById() {
+        given()
+                .cookies(cookies)
+                .pathParam("countryID", countryID)
+                .log().uri()
+
+                .when()
+                .delete("school-service/api/countries/{countryID}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteCountryById")
+    public void deleteCountryByIdNegative() {
+        given()
+                .cookies(cookies)
+                .pathParam("countryID", countryID)
+                .log().uri()
+
+                .when()
+                .delete("school-service/api/countries/{countryID}")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Country not found"))
+        ;
+    }
+
+    // task : Silinmiş bir ülkeyi update etmeye çalışınız : updateCountryNegative
+    @Test(dependsOnMethods = "deleteCountryById")
+    public void updateCountryNegative() {
+        countryName = getRandomName();
+        countryCode = getRandomCode();
+
+        Country country = new Country();
+        country.setId(countryID);
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .put("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Country not found"))
+        ;
+    }
+
+
+
+
+}
--- src/test/java/GoRest/GoRestUsersTests.java
+++ src/test/java/GoRest/GoRestUsersTests.java
@@ -1,17 +1,17 @@
 package GoRest;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Locale;
+import java.util.List;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
-
 import static org.hamcrest.Matchers.*;
 
 public class GoRestUsersTests {
@@ -20,70 +20,58 @@ public class GoRestUsersTests {
     User newUser;
 
     @BeforeClass
-    void Setup()
-    {
-        baseURI = "https://gorest.co.in/public/v2/users";
+    void Setup() {
+        baseURI = "https://gorest.co.in/public/v2/users"; // PROD
+        // baseURI = "https://test.gorest.co.in/public/v2/users";  // TEST
     }
 
+    public String getRandomName() {  return RandomStringUtils.randomAlphabetic(8); }
 
-    public String getRandomName()
-    {
-        return RandomStringUtils.randomAlphabetic(8);
-    }
+    public String getRandomEmail() { return RandomStringUtils.randomAlphabetic(8).toLowerCase()+"@gmail.com"; }
 
-    public String getRandomEmail()
-    {
-        return RandomStringUtils.randomAlphabetic(8).toLowerCase()+"@gmail.com";
-    }
-
-
-    @Test
+    @Test(enabled = false)
     public void createUserObject()
     {
-       // baslangic islemleri
-       // token aldim
-       // users JSON i hazirladim.
-
+        // başlangıç işlemleri
+        // token aldım
+        // users JSON ı hazırladım.
         int userID=
-        given()
-                .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
-                .contentType(ContentType.JSON)
-                .body("{\"name\":\""+getRandomName()+"\", \"gender\":\"male\", \"email\":\""+getRandomEmail()+"\", \"status\":\"active\"}")
-                // üst taraf request özellikleridir : hazirlik islemleri POSTMAN deki Authorization ve request BODY kismi
+                given()
+                        .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+                        .contentType(ContentType.JSON)
+                        .body("{\"name\":\""+getRandomName()+"\", \"gender\":\"male\", \"email\":\""+getRandomEmail()+"\", \"status\":\"active\"}")
+                        // üst taraf request özellikleridir : hazırlık işlemleri POSTMAN deki Authorization ve request BODY kısmı
+                        .log().uri()
+                        .log().body()
+                        .when() // request in olduğu nokta    POSTMAN deki SEND butonu
+                        .post("") //baseURI+paratez içi(http yoksa) respons un oluştuğu nokta
+                        // CREATE işlemi POST metodu ile çağırıyoruz POSTMAN deki gibi
 
-                .log().uri()
-                .log().body()
-                .when()  // request in oldugu nokta POSTMAN deki SEND butonu
-                .post("")  // respons un olustugu nokta CREATE islemi POST metodu ile cagiriyoruz POSTMAN deki gibi
-
-                // alt taraf response sonrasi POSTMAN deki test penceresi
-                .then()
-                .log().body()
-                .statusCode(201)
-                .contentType(ContentType.JSON)
-                .extract().path("id")
-        ;
+                        // alt taraf response sonrası  POSTMAN deki test penceresi
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id")
+                ;
 
         System.out.println("userID = " + userID);
-
     }
 
-
-    @Test
+    @Test(enabled = false)
     public void createUserObject2WithMap()
     {
-        Map<String,String> neuUser= new HashMap<>();
-        neuUser.put("name",getRandomName());
-        neuUser.put("gender","male");
-        neuUser.put("email",getRandomEmail());
-        neuUser.put("status","active");
+        Map<String,String> newUser=new HashMap<>();
+        newUser.put("name",getRandomName());
+        newUser.put("gender","male");
+        newUser.put("email",getRandomEmail());
+        newUser.put("status","active");
 
         int userID=
                 given()
                         .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
                         .contentType(ContentType.JSON)
-                        .body(neuUser)
-                        .log().uri()
+                        .body(newUser)
                         .log().body()
 
                         .when()
@@ -97,14 +85,12 @@ public class GoRestUsersTests {
                 ;
 
         System.out.println("userID = " + userID);
-
     }
-
 
     @Test
     public void createUserObject3WithObject()
     {
-        newUser= new User();
+        newUser=new User();
         newUser.setName(getRandomName());
         newUser.setGender("male");
         newUser.setEmail(getRandomEmail());
@@ -115,7 +101,6 @@ public class GoRestUsersTests {
                         .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
                         .contentType(ContentType.JSON)
                         .body(newUser)
-                        .log().uri()
                         .log().body()
 
                         .when()
@@ -126,45 +111,175 @@ public class GoRestUsersTests {
                         .statusCode(201)
                         .contentType(ContentType.JSON)
                         //.extract().path("id")
-                        .extract().jsonPath().getInt("id")
-                ;
-
+                        .extract().jsonPath().getInt("id");
+        ;
         System.out.println("userID = " + userID);
 
-        // path : class veya tip dönüsümüne imkan veremeyen direk veriyi verir. List<String> gibi
-        // jsonPath : class dönüsümüne ve tip dönüsümüne izin vererek, veriyi istedigimiz formatta verir.
+        // path : class veya tip dönüşümüne imkan veremeyen direk veriyi verir. List<String> gibi
+        // jsonPath : class dönüşümüne ve tip dönüşümüne izin vererek , veriyi istediğimiz formatta verir.
     }
 
-
-    @Test(dependsOnMethods = "createUserObject3WithObject")
+    @Test(dependsOnMethods = "createUserObject3WithObject", priority = 1)
     public void getUserByID()
     {
+        given()
+                .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+                .pathParam("userId",userID)
+                .log().uri()
 
+                .when()
+                .get("/{userId}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(userID))
+        ;
+    }
+
+    @Test(dependsOnMethods = "createUserObject3WithObject", priority = 2)
+    public void updateUserObject()
+    {
+        // newUser.setName("ismet temur");
+
+        Map<String,String> updateUser=new HashMap<>();
+        updateUser.put("name", "ismet temur");
+
+        given()
+                .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+                .pathParam("userId",userID)
+                .contentType(ContentType.JSON)
+                .body(updateUser)
+                .log().body()
+                .log().uri()
+
+                .when()
+                .put("/{userId}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(userID))
+                .body("name",equalTo("ismet temur"))
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateUserObject", priority = 3)
+    public void deleteUserById()
+    {
+
+        given()
+                .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+                .pathParam("userId",userID)
+                .log().uri()
+
+                .when()
+                .delete("/{userId}")
+
+                .then()
+                .log().body()
+                .statusCode(204)
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteUserById")
+    public void deleteUserByIdNegative()
+    {
+
+        given()
+                .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+                .pathParam("userId",userID)
+                .log().uri()
+
+                .when()
+                .delete("/{userId}")
+
+                .then()
+                .log().body()
+                .statusCode(404)
+        ;
+    }
+
+    @Test(enabled = false)
+    public void getUsers()
+    {
+        Response body=
                 given()
-
-                        .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
-                        .pathParam("userId", userID)
-                        .log().uri()
+                        .header("Authorization", "Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d")
 
                         .when()
-                        .get("/{userId}")
+                        .get()
+
+                        .then()
+                        //.log().body()
+                        .statusCode(200)
+                        .extract().response()
+                ;
+
+        // path ve jsonpath farkları
+        int idUser3path     =  body.path("[2].id");  // tip dönüşümü ototmatik uygun tip verilmeli
+        int idUser3JsonPath = body.jsonPath().getInt("[2].id"); // tip dönüşümü kendi içinde yapılabiliyor
+        System.out.println("idUser3path = " + idUser3path);
+        System.out.println("idUser3JsonPath = " + idUser3JsonPath);
+
+        User[] userlar= body.as(User[].class);  // extract.as
+        System.out.println("Arrays.toString(userlar) = " + Arrays.toString(userlar));
+
+        List<User> listUserlar = body.jsonPath().getList("", User.class);  // jsonpath ile list e dönüştürerek alabiliyorum
+        System.out.println("listUserlar = " + listUserlar);
+
+    }
+
+    @Test
+    public void getUsersV1() {
+        Response body =
+                given()
+                        .header("Authorization", "Bearer ad72d8d6e33e1a050a01d304c58b7bd711ba4d5ed735a0456ff40e1c1e6815ce")
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
 
                         .then()
                         .log().body()
                         .statusCode(200)
-                        .contentType(ContentType.JSON)
-                        .body("id", equalTo(userID))
-                ;
+                        .extract().response();
+
+        // body.as(), extract.as // tüm gelen response uygun nesneler için tüm classların yapılması gerekiyor.
+
+        List<User> dataUsers= body.jsonPath().getList("data", User.class);
+        // JSONPATH bir response içindeki bir parçayı nesneye ödnüştürebiliriz.
+        System.out.println("dataUsers = " + dataUsers);
+
+        // Daha önceki örneklerde (as) Clas dönüşümleri için tüm yapıya karşılık gelen
+        // gereken tüm classları yazarak dönüştürüp istediğimiz elemanlara ulaşıyorduk.
+        // Burada ise(JsonPath) aradaki bir veriyi clasa dönüştürerek bir list olarak almamıza
+        // imkan veren JSONPATH i kullandık.Böylece tek class ise veri alınmış oldu
+        // diğer class lara gerek kalmadan
+
+        // path : class veya tip dönüşümüne imkan veremeyen direk veriyi verir. List<String> gibi
+        // jsonPath : class dönüşümüne ve tip dönüşümüne izin vererek , veriyi istediğimiz formatta verir.
 
     }
+
 
 }
 
 class User{
+    private int id;
     private String name;
     private String gender;
     private String email;
     private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -196,6 +311,17 @@ class User{
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", status='" + status + '\'' +
+                '}';
     }
 
 
